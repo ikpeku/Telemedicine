7c45b08887c5b432413abb3e91d1bd61e4ba05cd
ikpeku <ikpedaniel07@gmail.com>
2023-05-13 18:33:03 +0100

    quiz dummy data

--- components/profile_com/PersonalProfile.jsx
+++ components/profile_com/PersonalProfile.jsx
@@ -1,22 +1,24 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { useForm } from "react-hook-form";
 import { Button, Input } from '../../static';
 import { List, RadioButton } from 'react-native-paper';
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
+import { userProvider } from '../../Context/UserProvider';
 
 const PersonalProfile = () => {
     const router = useRouter()
+    const { user } = useContext(userProvider)
 
     const [sex, setSex] = useState("male")
     const [DOB, setDOB] = useState()
 
     const { handleSubmit, control } = useForm({
         defaultValues: {
-            Full_Name: "",
-            Email: "",
-            Phone_Number: "",
+            Full_Name: user?.attributes?.name,
+            Email: user?.attributes?.email,
+            Phone_Number: user?.attributes?.phone_number,
             Address: "",
             Sex: "",
 
@@ -36,7 +38,7 @@ const PersonalProfile = () => {
         <>
             <Input control={control} name="Full_Name" placeholder="Enter Full Name" label="Full Name" rules={{ required: "required" }} />
             <Input control={control} name="Email" placeholder="Enter Email" label="Email" rules={{ required: "required" }} />
-            <Input control={control} name=" Phone_Number" placeholder="Enter  Phone Number" label=" Phone Number" rules={{ required: "required" }} />
+            <Input control={control} name="Phone_Number" placeholder="Enter  Phone Number" label=" Phone Number" rules={{ required: "required" }} />
             <Input control={control} name="Address" placeholder="Enter Address" label="Address" rules={{ required: "required" }} />
 
             <List.AccordionGroup >
@@ -63,7 +65,7 @@ const PersonalProfile = () => {
                     </View>
                 </List.Accordion>
 
-                <List.Accordion title="Sex Assigned at Birth" id="2" style={styles.container}
+                <List.Accordion title="Date of Birth" id="2" style={styles.container}
                     right={props => <MaterialCommunityIcons {...props} name="calendar-month" size={24} color="gainsboro" />}
                 >
 
---
+++ components/quiz.js
@@ -0,0 +1,166 @@
+export default [
+    {
+        id: "1",
+        topic: "Urinary tract infections",
+        treatment: "UTI TREATMENT",
+        appointment: "NO APPOINTMENT NEEDED",
+        about: "UTI can come with very unpleasant symptom and you need an immediate solution. Get UTI treatment from our medical experts. We will send your prescriptions to your doorstep",
+
+        questionaire: [
+            {
+                id: "1",
+                question: "What sex were you assigned to at birth",
+                instruction: "Choose one answer from the options",
+                options: ["Male", "Female"],
+                answer: [],
+                appointment: {
+                    pick: "male"
+                }
+            },
+            {
+                id: "2",
+                question: "Which of the following defines your condition",
+                instruction: "Choose one answer from the options",
+                options: ["Pregnant", "Nursing mother (breast feeding your baby)", "None of the above"],
+                answer: [],
+                appointment: {
+                    pick: "male"
+                }
+            },
+            {
+                id: "3",
+                question: "Have you had UTI in the past",
+                instruction: "Choose one answer from the options",
+                options: [
+                    "Yes",
+                    "No",
+                    "I don’t know"
+                ],
+                answer: [],
+                appointment: {
+                    pick: "male"
+                }
+            },
+            {
+                id: "4",
+                question: "How many times have you treated UTI In the past 6 months",
+                instruction: "Choose one answer from the options",
+                options: ["0", "1", "2"],
+                answer: [],
+                appointment: {
+                    pick: "male"
+                }
+            },
+
+            {
+                id: "5",
+                question: "If you were prescribed an antibiotic for UTI which of these antibiotics did you use",
+                instruction: "Choose one answer from the options",
+                options: [
+                    "Ciprofloxacin ",
+                    "Augmentin(Amoxicillin-Clavunate)",
+                    "Levofloxacin",
+                    "Trimethoprim-sulphamethazole",
+                    "Nitrofurantion",
+                    "I don’t know",
+                    "Other"
+                ],
+                answer: [],
+                appointment: {
+                    pick: "male"
+                }
+            },
+            {
+                id: "6",
+                question: "For the UTI Treatment did you complete your antibiotics",
+                instruction: "Choose one answer from the options",
+                options: [
+                    "No",
+                    "Yes and my symptoms disappeared",
+                    "Yes and my symptoms persisted",
+                    "I don’t know",
+                ],
+                answer: [""],
+                appointment: {
+                    pick: "male"
+                }
+            },
+            {
+                id: "7",
+                question: "What sex were you assigned to at birth",
+                instruction: "Choose one answer from the options",
+                options: ["Male", "Female"],
+                answer: [""],
+                appointment: {
+                    pick: "male"
+                }
+            },
+            {
+                id: "8",
+                question: "What sex were you assigned to at birth",
+                instruction: "Choose one answer from the options",
+                options: ["Male", "Female"],
+                answer: [""],
+                appointment: {
+                    pick: "male"
+                }
+            },
+            {
+                id: "9",
+                question: "What sex were you assigned to at birth",
+                instruction: "Choose one answer from the options",
+                options: ["Male", "Female"],
+                answer: [""],
+                appointment: {
+                    pick: "male"
+                }
+            },
+            {
+                id: "10",
+                question: "What sex were you assigned to at birth",
+                instruction: "Choose one answer from the options",
+                options: ["Male", "Female"],
+                answer: [""],
+                appointment: {
+                    pick: "male"
+                }
+            },
+            {
+                id: "11",
+                question: "What sex were you assigned to at birth",
+                instruction: "Choose one answer from the options",
+                options: ["Male", "Female"],
+                answer: [""],
+                appointment: {
+                    pick: "male"
+                }
+            },
+            {
+                id: "12",
+                question: "What sex were you assigned to at birth",
+                instruction: "Choose one answer from the options",
+                options: ["Male", "Female"],
+                answer: [""],
+                appointment: {
+                    pick: "male"
+                }
+            },
+        ]
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
